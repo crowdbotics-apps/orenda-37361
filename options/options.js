913b8e733d24d231151aeb6940c69ce385a3c8f7
Crowdbotics <team@crowdbotics.com>
2022-10-22 14:18:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "orenda_37361",
+  url: "https://orenda_37361.botics.co",
+  api: "https://orenda_37361.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
